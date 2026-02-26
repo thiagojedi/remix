0d95b9aa8310d9944922ca466e1fd3c8c44bf767
Thiago 'Jedi' Cerqueira <thiagoa7@gmail.com>
2026-02-25 23:06:12 -0300

    Use the same folder for sse demo

---
+++ demos/sse/deno.entry.ts
@@ -0,0 +1,10 @@
+import { router } from "./app/router.tsx";
+
+export default {
+  fetch: (req: Request) => router.fetch(req),
+  onListen: (address: { port: number }) => {
+    console.log(
+      `Server-Sent Events demo is running on http://localhost:${address.port}`,
+    );
+  },
+};
